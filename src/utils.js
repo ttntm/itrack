f34988ee68cbc27edb358e1e8d2076f30277a87f
ttntm <ttntm@pm.me>
2021-02-08 18:37:10 +0100

    add: support for hours

--- src/utils.js
+++ src/utils.js
@@ -3,13 +3,17 @@ import { useStore } from './store.js';
 const { appTheme } = useStore();
 
 export const formatTime = (input) => {
-  let min = input / 60;
+  let hrs = input / 3600;
+  hrs = hrs.toString().split('.')[0];
 
+  let min = (input / 60) - (hrs * 60);
   min = min.toString().split('.')[0];
 
-  let sec = input - (min * 60);
+  let sec = input - (min * 60) - (hrs * 3600);
 
-  return `${(min < 10 ? '0' : '') + min}:${(sec < 10 ? '0' : '') + sec}`
+  return hrs > 0 ?
+    `${(hrs < 10 ? '0' : '') + hrs}:${(min < 10 ? '0' : '') + min}:${(sec < 10 ? '0' : '') + sec}` :
+    `${(min < 10 ? '0' : '') + min}:${(sec < 10 ? '0' : '') + sec}`;
 }
 
 export const applyTheme = (theme) => {
